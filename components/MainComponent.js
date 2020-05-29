2286df56d875c27c9f8fe0c609bff1f52cc03ff7
James Grigoras <jamesgrigoras@yahoo.com>
2020-05-28 22:06:28 -0400

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfo'
-import { View , Platform} from 'react-native'
-import { createStackNavigator} from 'react-navigation'
-
+import CampsiteInfo from './CampsiteInfo';
+import { View, Platform } from 'react-native';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
-        Directory: {screen: Directory},
-        CampsiteInfo: {screen: CampsiteInfo}
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
     },
     {
         initialRouteName: 'Directory',
@@ -22,17 +22,45 @@ const DirectoryNavigator = createStackNavigator(
             }
         }
     }
-)
+);
+
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
 class Main extends Component {
     render() {
         return (
             <View style={{
-                flex:1,
+                flex: 1,
                 paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
             }}>
-               <DirectoryNavigator />
+                <MainNavigator />
             </View>
-            );
+        );
     }
 }
 
